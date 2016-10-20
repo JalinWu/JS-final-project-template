a3549e887bf8686f7451fae82107944192568fa7
JalinWu <jalin820305@gmail.com>
2016-10-21 01:53:55 +0800

    add enemy

--- main.js
+++ main.js
@@ -5,8 +5,16 @@ var canvas = document.getElementById("game-canvas");
 //取得2D繪圖用的物件
 var ctx = canvas.getContext("2d");
 
+var enemyImg = document.createElement('img');
+enemyImg.src = "images/slime.gif";
+var enemy = {
+  x = 96,
+  y = 480
+};
+
 function draw() {
   ctx.drawImage(bgImg, 0, 0);
+  ctx.drawImage(enemyImg, enemy.x, enemy.y);
 }
 
 setTimeout(draw, 1000);
