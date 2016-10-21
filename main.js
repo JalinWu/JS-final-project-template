f495dc35143c3e74b7bc44d757ed1257c150c718
JalinWu <jalin820305@gmail.com>
2016-10-21 10:08:08 +0800

    add tower

--- main.js
+++ main.js
@@ -1,10 +1,6 @@
 var bgImg = document.createElement('img');
 bgImg.src = "images/map.png";
 
-var canvas = document.getElementById("game-canvas");
-//取得2D繪圖用的物件
-var ctx = canvas.getContext("2d");
-
 var enemyImg = document.createElement('img');
 enemyImg.src = "images/slime.gif";
 var enemy = {
@@ -12,9 +8,21 @@ var enemy = {
   y: 480-32
 };
 
+var towerImg = document.createElement('img');
+towerImg.src = "images/tower-btn.png";
+var tower = {
+  x: 680-32,
+  y: 480-32
+};
+
+var canvas = document.getElementById("game-canvas");
+//取得2D繪圖用的物件
+var ctx = canvas.getContext("2d");
+
 function draw() {
   ctx.drawImage(bgImg, 0, 0);
   ctx.drawImage(enemyImg, enemy.x, enemy.y);
+  ctx.drawImage(towerImg, tower.x, tower.y);
 }
 
 setInterval(draw, 16);
