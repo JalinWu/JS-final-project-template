28493bd274bc7ac2db1554f8ba996db95d8d0658
JalinWu <jalin820305@gmail.com>
2016-10-31 14:12:21 +0800

    Update main.js

--- main.js
+++ main.js
@@ -22,7 +22,7 @@ towerImg.src = "images/tower.png";
 
 var cursor = {};
 $("#game-canvas").on("mousemove",function(event){
-  conlose.log("x: " + event.offsetX + " y: " + event.offsetY);
+  console.log("x: " + event.offsetX + " y: " + event.offsetY);
   cursor = {
     x: event.offsetX,
     y: event.offsetY
