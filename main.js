3d9434018a7dcb79c597c3e69e365c371aa1e13d
JalinWu <jalin820305@gmail.com>
2016-11-07 21:41:03 +0800

    Update main.js

--- main.js
+++ main.js
@@ -9,11 +9,47 @@ var enemy = {
   y: 480-32,
   speedX: 0,
   speedY: -64,
-  move: function() {
-    this.x += this.speedX/FPS;
-    this.y += this.speedY/FPS;
-  }
+  move: function(){
+        if( isCollided(enemyPath[this.pathDes].x, enemyPath[this.pathDes].y, this.x, this.y, 64/FPS, 64/FPS) ){
+
+            // 首先，修正位置到目標路徑點
+            this.x = enemyPath[this.pathDes].x;
+            this.y = enemyPath[this.pathDes].y;
+
+            // 指定下一個路徑點為目標路徑點
+            this.pathDes++;
+
+            // 重新設定設定前往目標路徑點的所需的水平/垂直速度
+            if (enemyPath[this.pathDes].x>this.x) {
+              this.speedX = 64;
+              this.speedY = 0;
+            } else if (enemyPath[this.pathDes].x<this.x) {
+              this.speedX = -64;
+              this.speedY = 0;
+            } else if (enemyPath[this.pathDes].y>this.y) {
+              this.speedX = 0;
+              this.speedY = 64;
+            } else if (enemyPath[this.pathDes].y<this.y) {
+              this.speedX = 0;
+              this.speedY = -64;
+            }
+
+        } else {
+            this.x = this.x + this.speedX/FPS;
+            this.y = this.y + this.speedY/FPS;
+        }
+    }
 };
+
+var enemyPath = [
+    {x:96, y:64},
+    {x:384, y:64},
+    {x:384, y:192},
+    {x:224, y:192},
+    {x:224, y:320},
+    {x:544, y:320},
+    {x:544, y:96}
+];
 
 var towerBtnImg = document.createElement('img');
 towerBtnImg.src = "images/tower-btn.png";
@@ -72,4 +108,14 @@ function draw() {
 
 setInterval(draw, 1000/FPS);
 
-
+function isCollided(pointX, pointY, targetX, targetY, targetWidth, targetHeight) {
+    if(     pointX >= targetX
+        &&  pointX <= targetX + targetWidth
+        &&  pointY >= targetY
+        &&  pointY <= targetY + targetHeight
+    ){
+        return true;
+    } else {
+        return false;
+    }
+}
